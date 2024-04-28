c2a2cd55f9e14d52b2472170ed05770bd1bc90fe
Carlos Barriga <cabarriga27qgmail.com>
2024-04-27 19:55:24 -0500

    Updating the methods and composition of the Product and Catalogue classes

--- Final_proyect/Documentation/src/catalogue.py
+++ Final_proyect/Documentation/src/catalogue.py
@@ -1,12 +1,45 @@
 """This file has the classes that represents the catalogue of the plattform"""
+
+from product import Product
+
+
 class Catalogue:
-    """This class represent a catalogue"""
-    def __init__(self, id_catalogo:str) -> None:
+    
+    """This class represent a catalogue of products"""
+
+    products = []
+
+    
+    def __init__(self, id_catalogo: str) -> None:
         self.id_catalogo = id_catalogo
 
-    def show_list_products(self):
-        """This method is used to show a list of products """
+    @classmethod
+    def show_list_products(cls):
+        """This method is used to show a list of products"""
+        return cls.products
 
+    @classmethod
+    def show_products_by_name(cls, name: str):
+        """This method is used to search a product by name"""
+        return [product for product in cls.products if product.name == name]
 
-    def search_products(self):
-        """This method is used to search a product"""
+    @classmethod
+    def show_products_by_department(cls, department: str):
+        """This method is used to search a product by department"""
+        return [
+            product for product in cls.products if product.department == department
+        ]
+
+    @classmethod
+    def add_product(cls, product: Product, ):
+        """This method is used to add a product to the vidogame list"""
+        cls.products.append(product)
+
+    @classmethod
+
+    def get_product(cls, id:str) -> Product:
+        """This method is used to get a product"""
+        for product in cls.products:
+            if product.id == id:
+                return product
+        return None
