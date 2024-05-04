33abdffa1dbd5c5ad401d7947b2da76057372713
Carlos Barriga <cabarriga27qgmail.com>
2024-05-04 18:10:13 -0500

    Creation of the services folder

---
+++ Final_proyect/src/services/services.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from catalogue_basemodel import Catalogue
+from product_basemodel import Product
+from users_basemodel import User
+from users_basemodel import Customer    
+from users_basemodel import Admin
+
+app = FastAPI()
+
