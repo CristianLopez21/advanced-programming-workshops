1374892ec8fbf1f634bac1beb42563ce81ce57df
Carlos Barriga <cabarriga27qgmail.com>
2024-05-04 16:50:38 -0500

    Git test

--- Final_proyect/src/payment.py
+++ Final_proyect/src/payment.py
@@ -1,7 +1,7 @@
 """This file has the classes that represents the payment gategay of the plattform"""
 
 class PaymentGateway:
-    """This class a Payment gateway"""
+    """This class is a Payment gateway """
     def __init__(self) -> None:
         self.login_status = None
         
