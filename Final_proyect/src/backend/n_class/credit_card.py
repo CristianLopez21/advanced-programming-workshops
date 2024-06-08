3a96ad6a554e4d36c0d11dc776e50bc57cf0ec20
Carlos Barriga <cabarriga27qgmail.com>
2024-06-08 11:36:41 -0500

    Update of the backend and frontend

--- Final_proyect/src/backend/n_class/credit_card.py
+++ Final_proyect/src/backend/n_class/credit_card.py
@@ -1,24 +1,17 @@
 """
 To do: doc string
 """
-from .users import Customer
-class CreditCard:
+from pydantic import BaseModel
+class CreditCard(BaseModel):
     """This class represents a credit card"""
 
-    def __init__(self, user_email:Customer , number: int, due_date: str, cvv: int, name: str):
-        """
-        Contructor of the class
+    user_email : str
+    number : str
+    due_date : str
+    cvv : str
+    name : str
 
-        Parameters:
-
-        - number(int): Number of the credit card.
-        - due_data(str): Due data of the credit card.
-        - cvv(int): Code cvv of the credit car  d.
-        - name (str): Name of the credit card.
+    """
+    T  This class represents a credit card
         
-        """
-        self.user_email = user_email
-        self.number = number
-        self.due_date = due_date
-        self.cvv = cvv
-        self.name = name
+    """
