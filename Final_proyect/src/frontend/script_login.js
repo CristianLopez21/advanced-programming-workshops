83182cff31aedf8edacf4fc2383346b3ab74efd4
Carlos Barriga <cabarriga27qgmail.com>
2024-06-10 18:45:09 -0500

    Frontend update

--- Final_proyect/src/frontend/script_login.js
+++ Final_proyect/src/frontend/script_login.js
@@ -1,6 +1,7 @@
 document.getElementById('logout_button').addEventListener('click', function() {
     window.location.href = 'login.html'; 
 });
-document.getElementById('shoppingcar_button').addEventListener('click', function() {
-    window.location.href = 'shopping_car.html'; 
+document.getElementById('shoppingcart_button').addEventListener('click', function() {
+    window.location.href = 'shopping_cart.html'; 
 });
+
